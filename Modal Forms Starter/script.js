b181b74a5e1d28a1a52381aa152047589e66cb74
yourUserName <patrick.mack1@yahoo.com>
2019-05-28 22:18:06 +0100

    Update script.js

--- Modal Forms Starter/script.js
+++ Modal Forms Starter/script.js
@@ -7,10 +7,20 @@ const loginBtn = document.querySelector('.login-btn');
 const signupX = document.querySelector('.signup-x');
 const loginX = document.querySelector('.login-x');
 
+
+
 signupBtn.addEventListener('click', () => {
     signupModal.classList.add('display')
 });
 
 loginBtn.addEventListener('click', () => {
     loginModal.classList.add('display')
+});
+
+signupX.addEventListener('click', () => {
+    signupModal.classList.remove('display')
+});
+
+loginX.addEventListener('click', () => {
+    loginModal.classList.remove('display')
 });
