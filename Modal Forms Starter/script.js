67aee7455da7b498ecf1b626206b6268295b801e
yourUserName <patrick.mack1@yahoo.com>
2019-05-28 22:13:09 +0100

    Update script.js

---
+++ Modal Forms Starter/script.js
@@ -0,0 +1,4 @@
+const signupModal = document.querySelector(".signup-form-wrapper");
+const loginModal = document.querySelector(".login-form-wrapper");
+const signupBtn = document.querySelector('.signup-btn');
+const loginBtn = document.querySelector('.login-btn');
