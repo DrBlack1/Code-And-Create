42b7d3fe26cb7787566f3ba83d6bb3b449e31ea7
yourUserName <patrick.mack1@yahoo.com>
2019-05-29 22:29:49 +0100

    Update gulpfile.js

--- GulpJs/gulp-website/gulpfile.js
+++ GulpJs/gulp-website/gulpfile.js
@@ -1,1 +1,21 @@
 var gulp = require('gulp');
+
+// Styles
+gulp.task('styles', function () {
+    console.log('starting styles task')
+});
+
+// Scripts
+gulp.task('scripts', function () {
+    return gulp.src('package.json')
+    console.log('starting script task')
+});
+
+// Images
+gulp.task('images', function () {
+    console.log('starting images task')
+});
+
+gulp.task('default', function () {
+    console.log('Starting default task')
+});
