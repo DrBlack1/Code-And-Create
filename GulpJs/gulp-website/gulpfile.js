5efcddde809480d9627a6659d0527cd526b10761
yourUserName <patrick.mack1@yahoo.com>
2019-05-29 23:14:33 +0100

    Updated gulpfile.js

--- GulpJs/gulp-website/gulpfile.js
+++ GulpJs/gulp-website/gulpfile.js
@@ -2,33 +2,33 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
 // File paths
-var SCRIPTS_PATH = 'public/scripts/**/*.js';
+var SCRIPTS_PATH = 'public/scripts/**/*.js'
 
 // Styles
 gulp.task('styles', function () {
-    console.log('starting styles task')
+	console.log('starting styles task');
 });
 
 // Scripts
 gulp.task('scripts', function () {
-    console.log('starting script task')
+	console.log('starting scripts task');
 
-    return gulp.src(SCRIPTS_PATH)
-        .pipe(uglify())
-        .pipe(gulp.dest('public/dist'));
+	return gulp.src(SCRIPTS_PATH)
+		.pipe(uglify())
+		.pipe(gulp.dest('public/dist'));
 });
 
 // Images
 gulp.task('images', function () {
-    console.log('starting images task')
+	console.log('starting images task');
 });
 
 gulp.task('default', function () {
-    console.log('Starting default task')
+	console.log('Starting default task');
 });
 
 gulp.task('watch', function () {
-    console.log('Starting watch task')
-    require('./server.js')
-    gulp.watch(SCRIPTS_PATH, ['scripts'])
+	console.log('Starting watch task');
+	require('./server.js');
+	gulp.watch(SCRIPTS_PATH, ['scripts']);
 });
